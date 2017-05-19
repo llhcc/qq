4deffa9ec04d8d6869bf36b14cee8d9265c9e400
llhcc <llhcc4137@sina.com>
2017-05-19 17:47:41 +0800

    cccccc

--- qq/src/main/java/com/llh/qq/Test.java
+++ qq/src/main/java/com/llh/qq/Test.java
@@ -11,13 +11,13 @@ public class Test {
 
 /////S344456677669966666666666
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 /////S3444656789900-9666666666666
 //>>>>>>> branc789h 'master' of 00https://github.com/llhcc/qq.git
-=======
+//=======
 /////S344465678999666666666666
 //>>>>>>> branch 'master' of https://github.com/llhcc/qq.git
->>>>>>> branch 'master' of https://github.com/llhcc/qq.git
+//>>>>>>> branch 'master' of https://github.com/llhcc/qq.git
 		//----==
 	///	6666
 		
